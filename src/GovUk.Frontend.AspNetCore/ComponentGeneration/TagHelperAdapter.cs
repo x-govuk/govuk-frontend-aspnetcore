e14910e03961a562dcfd803261e9ef84ac96061e
James Gunn <james@gunn.io>
2025-07-19 11:47:19 +0100

    Revert "Remove HtmlMonkey dependency"
    Issue with nested <table>s is fixed in latest version.
    
    This reverts commit 6e63f3508b51a764273a7b840d80988e6bec5603.

--- src/GovUk.Frontend.AspNetCore/ComponentGeneration/TagHelperAdapter.cs
+++ src/GovUk.Frontend.AspNetCore/ComponentGeneration/TagHelperAdapter.cs
@@ -1,6 +1,7 @@
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using SoftCircuits.HtmlMonkey;
 
 namespace GovUk.Frontend.AspNetCore.ComponentGeneration;
 
@@ -39,67 +40,16 @@ internal static class TagHelperAdapter
             return ComponentTagHelperOutput.Empty;
         }
 
-        // This is a roughly good enough HTML parser that lets us extract the root tag and its attributes.
-        // It's certainly not fully-compliant but it's enough for the components that we generate.
+        var doc = HtmlDocument.FromHtml(html);
+        var root = (HtmlElementNode)doc.RootNodes.Single();
 
-        var rootStartTagStartsAt = html.IndexOf('<', 0);
-        var rootStartTagNameEndsAt = html.IndexOfAny([' ', '/', '>'], rootStartTagStartsAt) - 1;
-        var rootStartTagEndsAt = html.IndexOf('>', rootStartTagStartsAt + 1);
+        var tagName = root.TagName;
+        var tagMode = root.IsSelfClosing ? TagMode.SelfClosing : TagMode.StartTagAndEndTag;
+        var attributes = new TagHelperAttributeList(
+            root.Attributes.Select(a => a.Value is null ? new TagHelperAttribute(a.Name) : new TagHelperAttribute(a.Name, new HtmlString(a.Value))));
+        var innerHtml = new HtmlString(root.InnerHtml);
 
-        var rootTagName = html.Substring(rootStartTagStartsAt + 1, rootStartTagNameEndsAt - rootStartTagStartsAt);
-        var isSelfClosing = html[rootStartTagEndsAt - 1] == '/';
-
-        var attributeList = html
-            .Substring(rootStartTagNameEndsAt + 1, html.LastIndexOf('>', rootStartTagEndsAt) - rootStartTagNameEndsAt - 1)
-            .TrimStart(' ')
-            .TrimEnd('/', ' ');
-
-        var attributes = new TagHelperAttributeList();
-        for (var i = 0; i < attributeList.Length;)
-        {
-            var endOfAttributeName = attributeList.IndexOfAny(['=', ' '], i) - 1;
-            if (endOfAttributeName == -2)
-            {
-                endOfAttributeName = attributeList.Length + 1;
-            }
-
-            var attributeName = attributeList.Substring(i, endOfAttributeName - i + 1);
-            i += attributeName.Length + 1;
-
-            if (attributeList[endOfAttributeName + 1] == '=')
-            {
-                var startOfAttributeValue = endOfAttributeName + 3;
-                var endOfAttributeValue = attributeList.IndexOf('"', startOfAttributeValue + 1);
-                var attributeValue = endOfAttributeValue != -1 ?
-                    attributeList.Substring(startOfAttributeValue, endOfAttributeValue - startOfAttributeValue) :
-                    string.Empty;
-                i += attributeValue.Length + 3;
-
-                attributes.Add(new TagHelperAttribute(attributeName, new HtmlString(attributeValue), HtmlAttributeValueStyle.DoubleQuotes));
-            }
-            else
-            {
-                attributes.Add(new TagHelperAttribute(attributeName, null, HtmlAttributeValueStyle.Minimized));
-            }
-        }
-
-        string innerHtml = string.Empty;
-        if (!isSelfClosing)
-        {
-            var rootEndTagStartsAt = html.LastIndexOf("</", StringComparison.InvariantCulture);
-            if (rootEndTagStartsAt == -1)
-            {
-                isSelfClosing = true;
-            }
-            else
-            {
-                innerHtml = html.Substring(rootStartTagEndsAt + 1, rootEndTagStartsAt - rootStartTagEndsAt - 1);
-            }
-        }
-
-        var tagMode = isSelfClosing ? TagMode.SelfClosing : TagMode.StartTagAndEndTag;
-
-        return new ComponentTagHelperOutput(rootTagName, tagMode, attributes, new HtmlString(innerHtml));
+        return new ComponentTagHelperOutput(tagName, tagMode, attributes, innerHtml);
     }
 
     internal record ComponentTagHelperOutput(
@@ -108,11 +58,12 @@ internal static class TagHelperAdapter
         ReadOnlyTagHelperAttributeList Attributes,
         IHtmlContent InnerHtml)
     {
-        private static readonly ReadOnlyTagHelperAttributeList _emptyAttributes = new TagHelperAttributeList();
+        public static ReadOnlyTagHelperAttributeList EmptyAttributes { get; } =
+            new TagHelperAttributeList();
 
-        private static readonly IHtmlContent _emptyContent = new HtmlString("");
+        public static IHtmlContent EmptyContent { get; } = new HtmlString("");
 
         public static ComponentTagHelperOutput Empty { get; } =
-            new(null, TagMode.StartTagAndEndTag, _emptyAttributes, _emptyContent);
+            new(null, TagMode.StartTagAndEndTag, EmptyAttributes, EmptyContent);
     }
 }
